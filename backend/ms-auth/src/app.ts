0493ffedcb571c6f30466ce5459dcd7ea4682504
Ismail ASSIL <ismailassil00@gmail.com>
2025-05-11 19:32:39 +0100

    api-gateway

---
+++ backend/ms-auth/src/app.ts
@@ -0,0 +1,14 @@
+import fastify, { FastifyInstance } from 'fastify';
+
+const app: FastifyInstance = fastify({
+	logger: {
+		transport: {
+			target: 'pino-pretty',
+			options: {
+				colorize: true,
+			},
+		},
+	},
+});
+
+export { app };
--- backend/ms-auth/src/models/auth.model.ts
+++ backend/ms-auth/src/models/auth.model.ts
@@ -8,7 +8,7 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dbPath = path.join(__dirname, '../../../database/database.sqlite');
+const dbPath = path.join(__dirname, '../../database/database.sqlite');
 
 const databasePlugin = fp(async function (fastify: FastifyInstance) {
 	const database = await open({
