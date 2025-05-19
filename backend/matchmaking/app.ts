cdb82309420eb495bf182c746264902d2ff7f114
Salah Eddine Moumni <smoumni@e3r4p16.1337.ma>
2025-05-19 16:08:07 +0100

    building dummy routes

--- backend/matchmaking/app.ts
+++ backend/matchmaking/app.ts
@@ -1,10 +1,23 @@
 import fastify from "fastify"
 
-const app = fastify({ logger: true });
-
-app.get("/", async () => {
-
+const serverOptions = {
+    logger: true,
 }
-)
+
+const app = fastify(serverOptions);
+
+app.route({
+    url: '/hello',
+    method: "GET",
+    handler: function myHandler(req, res) {
+        
+        res.send("World")
+    }
+});
+
+app.get("/world", function handler(req, res) {
+
+    return { helloFrom: this.server.address() }
+})
 
 export default app;
