0a5bd5a999b0a07b232ee70024bd1473336adea4
Ismail ASSIL <ismailassil00@gmail.com>
2025-05-27 17:02:42 +0100

    adding metrics to backend through api-gateway and with a cutsom nodejs-exporter

---
+++ monitoring/backend-exporter/src/index.ts
@@ -0,0 +1,43 @@
+import { app as fastify } from "./app.js";
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
+
+let cached_metrics = "";
+
+const authHeader = "Basic " + Buffer.from("").toString("base64");
+
+// Retrive metrics every 5s
+setInterval(async () => {
+	try {
+		const res = await axios.get("http://localhost:4004/metrics", {
+			auth: {
+				// TODO: .env
+				username: "",
+				password: "",
+			},
+		});
+		cached_metrics = res.data;
+		fastify.log.info("Fetched latest metrics data");
+	} catch (err) {
+		fastify.log.error(err);
+	}
+}, 5000);
+
+fastify.get("/metrics", (req, rep) => {
+	rep.header("content-Type", "text-plain; version=0.0.4");
+	return cached_metrics || "# No metrics available yet";
+});
+
+async function main() {
+	try {
+		const address = await fastify.listen({ host: "::", port: 9434 }); // TODO: into .env
+		fastify.log.info(`Server is running at ${address}`);
+	} catch (error) {
+		fastify.log.error(error);
+		process.exit(1);
+	}
+}
+
+main();
