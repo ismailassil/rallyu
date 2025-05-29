852fd013937013fc60cc861b9850eb1a88e0e572
Ismail ASSIL <ismailassil00@gmail.com>
2025-05-29 21:07:43 +0100

    adding the ilm + backup

---
+++ logging/elasticsearch-policies/init/entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+RED="\033[31m"
+GREEN="\033[32m"
+CYAN="\033[36m"
+RESET="\033[0m"
+
+until curl -u ${ELASTIC_USERNAME}:${ELASTIC_PASSWORD} \
+	-X GET 'http://localhost:9200/_cluster/health?pretty' |
+	grep -q '\"status\" : \"green\"'; do
+	printf "${CYAN}Waiting for Elasticsearch to be ready...${RESET}\n"
+	sleep 5
+done
+
+## Register Snapshot repository
+## To trigger a snapshot to save your current indices you have to plan...
+###
+### curl ... -X PUT "http://elasticsearch:9200/_snapshot/my_backup/snapshot_$(date +%Y%m%d%H%M%S)?wait_for_completion=true"
+###
+# curl -u ${ELASTIC_USERNAME}:${ELASTIC_PASSWORD} \
+# 	-X PUT "http://elasticsearch:9200/_snapshot/my_backup" \
+# 	-H 'Content-Type: application/json' \
+# 	-d '{
+# 		"type": "fs", # file system
+# 		"settings": {
+# 			"location": "/usr/share/elasticsearch/backups",
+# 			"compress": true
+# 		}
+# 	}'
+
+## Create ILM Policy
+curl -u ${ELASTIC_USERNAME}:${ELASTIC_PASSWORD} \
+	-X PUT "http://elasticsearch:9200/_ilm/policy/logs_policy" \
+	-H 'Content-Type: application/json' \
+	--date @/init/conf/ilm-policy.json
+
+## Apply index template
+curl -u ${ELASTIC_USERNAME}:${ELASTIC_PASSWORD} \
+	-X PUT "http://elasticsearch:9200/_template/logs_template" \
+	-h 'Content-Type: application/json' \
+	--date @/init/conf/index-template.json
