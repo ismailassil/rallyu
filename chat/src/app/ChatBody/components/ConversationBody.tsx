7b414a9bb2bdc81033a151e256554d1232aa412f
Salah Demnati <sdemnati@e1r13p4.1337.ma>
2025-06-02 17:01:40 +0100

    fix scroll in muliMessage

--- chat/src/app/ChatBody/components/ConversationBody.tsx
+++ chat/src/app/ChatBody/components/ConversationBody.tsx
@@ -2,9 +2,32 @@
 import React, { useState } from 'react'
 import Image from "next/image"
 import user from "../../Users.json"
-import Message from './Message'
+
+type MessageType = {
+	content: string
+	date: string
+}
 
 const ConversationBody = () => {
+
+	const [message, setMessage] = useState("")
+	const [messages, setMessages] = useState<MessageType[]>([])
+	const sendData = () => {
+		const newMessage: MessageType = {
+			content: message.trim(),
+			date: "Monday 22:25"
+		}
+		setMessages(prev => [...prev, newMessage])	
+		setMessage("")
+	}
+
+	const handleEnterPress = (e: KeyboardEvent) => {
+		if (e.key === "Enter")
+		{
+			e.preventDefault()
+			sendData()
+		}
+	}
 	return (
 		<div className=" w-11/12 border h-full border-white/30 rounded-lg flex flex-col ">
 
@@ -23,7 +46,14 @@ const ConversationBody = () => {
 			</div>
 			{/* message body */}
 			<div className='overflow-auto custom-scrollbar p-4'>
-				<Message />
+				{messages.map((msg, index) => (
+					<div key={index} className='max-w-max w-9/12 border-white/30 border p-3 rounded-lg ml-auto mb-10 relative'>
+						<span className=''>{msg.content}</span>
+						<span className='-bottom-6 right-0 absolute text-white/30 text-sm w-32 text-end'>
+							{msg.date}
+						</span>
+					</div>
+				))}
 			</div>
 			{/* typing message and send */}
 			<div className='border-t border-t-white/30 p-4 mt-auto'>
@@ -31,9 +61,12 @@ const ConversationBody = () => {
 					<input
 						type='text'
 						placeholder="Enter your message"
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+						onKeyDown={handleEnterPress}
 						className=' focus:outline-none bg-transparent flex justify-around placeholder:text-lg placeholder:text-white/50 w-full' />
 					<Image 	width={20} height={20} src={"/icons/send.svg"} alt='send image'
-							className='hover:cursor-pointer'/>
+							className='hover:cursor-pointer' onClick={sendData}/>
 				</div>
 			</div>
 		</div>
