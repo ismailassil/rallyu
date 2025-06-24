4a12c59e90a99d5b3f952f2423f0df0f1d1b3c9e
Salah Demnati <sdemnati@e3r11p2.1337.ma>
2025-06-24 15:52:48 +0100

    clean session

---
+++ chat/src/app/components/Main/Message.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import message from "../../Messages.json"
+
+const Message = () => {
+  return (
+	<ul className="">
+		{Array.from({ length: 1 }).map((_, i) => (
+			<li key={i} className='max-w-max w-9/12 border-white/30 border p-3 rounded-lg ml-auto mb-10 relative'>
+				<span className=''>{message.content}</span>
+				<span className=' -bottom-6 right-0 absolute text-white/30 text-sm  w-32 text-end'>{message.date}</span>
+			</li>
+		))}
+	</ul>
+  )
+}
+
+export default Message
+
+
+
+// import React from 'react'
+// import message from "../../Messages.json"
+
+// const Message = () => {
+//   return (
+//   <>
+//     <span className=''>{message.content}</span>
+//     <span className=' -bottom-6 right-0 absolute text-white/30 text-sm  w-32 text-end'>{message.date}</span>
+//   </>
+//   )
+// }
+
+// export default Message
+//       // <li  className='max-w-max w-9/12 border-white/30 border p-3 rounded-lg ml-auto mb-10 relative'>
