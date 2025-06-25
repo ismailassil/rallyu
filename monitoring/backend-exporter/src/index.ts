558ed40e8e8d68404f3cf19fe3aa58728440a2ba
Ismail ASSIL <ismailassil00@gmail.com>
2025-06-25 11:49:51 +0100

    - Modifying API_GATEWAYn HTTP_PROXY and RATE_LIMITn - Adjusting the files structure

--- monitoring/backend-exporter/src/index.ts
+++ monitoring/backend-exporter/src/index.ts
@@ -6,31 +6,40 @@ dotenv.config();
 
 let cached_metrics = "";
 
+const GATEWAY_PORT = process.env.GATEWAY_PORT;
+
 // Retrive metrics every 5s
 setInterval(async () => {
 	try {
-		const res = await axios.get("http://api-gateway:4004/inter-metrics", {
-			auth: {
-				// TODO: .env
-				username: "admin",
-				password: "password",
-			},
-		});
+		const res = await axios.get(
+			`http://api-gateway:${GATEWAY_PORT}/inter-metrics`,
+			{
+				auth: {
+					username: `${process.env.USER}`,
+					password: `${process.env.PASSWORD}`,
+				},
+			}
+		);
 		cached_metrics = res.data;
 		fastify.log.info("Fetched latest metrics data");
 	} catch (err) {
 		fastify.log.error(err);
 	}
-}, 5000);
+}, 5000); // 5s
 
 fastify.get("/metrics", (req, rep) => {
 	rep.header("content-Type", "text/plain; version=0.0.4");
 	return cached_metrics || "# No metrics available yet";
 });
 
+const PORT = parseInt(process.env.PORT || "9434");
+
 async function main() {
 	try {
-		const address = await fastify.listen({ host: "::", port: 9434 }); // TODO: into .env
+		const address = await fastify.listen({
+			host: "::",
+			port: PORT,
+		});
 		fastify.log.info(`Server is running at ${address}`);
 	} catch (error) {
 		fastify.log.error(error);
