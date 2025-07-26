b6cc659ba381cdaeb78aa5dcb3cbd6a501d0b9fd
Ismail Assil <ismailassil00@gmail.com>
2025-07-26 11:32:52 +0100

    fix backend struct

--- app/backend/ms-auth/src/models/database.d.ts
+++
@@ -1,8 +0,0 @@
-import 'fastify';
-import { Database as SQLiteDatabase } from 'sqlite';
-
-declare module 'fastify' {
-	interface FastifyInstance {
-		database: SQLiteDatabase;
-	}
-}
--- app/backend/ms-chat/src/shared/exceptions/UserNotFoundException.ts
+++
@@ -1,8 +0,0 @@
-class UserNotFoundException extends Error {
-	constructor(message?: string) {
-		super(message || 'User Not Found');
-		this.name = 'User Not Found Exception';
-	}
-}
-
-export default UserNotFoundException;
