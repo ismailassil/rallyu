935e4b1b64e26efc1a98308f03ad156730190d42
Ismail Assil <ismailassil00@gmail.com>
2025-07-30 22:10:45 +0100

    fix gateway, add NATS to chat and adjust the notification

--- app/backend/ms-chat/app.js
+++ app/backend/ms-chat/app.js
@@ -23,46 +23,7 @@ await fastify.register(natsPlugin, {
 	NATS_PASSWORD: process.env.NATS_PASSWORD,
 });
 
-// fastify.ready().then(() => {
-//   const io = fastify.io;
-
-//   io.on('connection', (socket) => {
-//     console.log(`Client connected: ${socket.id}`);
-
-//     socket.on('getM', (data) => {
-//       console.log(`Received getM: ${data}`);
-//       const stmt = fastify.db.prepare('SELECT * FROM message ORDER BY id ASC');
-//       const messages = stmt.all();
-//       socket.emit('messageReceived', messages);
-//     });
-
-//     socket.on('sendM', (data) => {
-//       console.log(`Received sendM:`, data);
-//       const stmt = fastify.db.prepare('INSERT INTO message (senderId, receiverId, text) VALUES (?, ?, ?)');
-//       const result = stmt.run(data.senderId, data.receiverId, data.text);
-
-//       io.emit('newMessage', {
-//         id: result.lastInsertRowid,
-//         senderId: data.senderId,
-//         receiverId: data.receiverId,
-//         text: data.text
-//       });
-
-//     });
-//     socket.on("lastMessage", () => {
-//       const stmt = fastify.db.prepare('SELECT * FROM message WHERE id = (SELECT MAX(id) FROM message);');
-//       const lastMessage = stmt.get(); // executes the query and returns a single row
-//       // console.log("======>", JSON.stringify(lastMessage, null, 2));
-//       socket.emit("lastMessageResult", lastMessage); // send it back to the client
-//     });
-
-//     socket.on('disconnect', () => {
-//       console.log(`Client disconnected: ${socket.id}`);
-//     });
-//   });
-// });
-
-fastify.listen({ port: 4657, host: '0.0.0.0' }, (err) => {
+fastify.listen({ port: 4657, host: '::' }, (err) => {
 	if (err) {
 		fastify.log.error(err);
 		process.exit(1);
