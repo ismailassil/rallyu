762dee150ec40e1c88c1f22126d56caf1697d4e6
AmineMaila <aminemaila18@gmail.com>
2025-08-03 23:13:08 +0100

    added animated queue button

--- app/frontend/src/app/(onsite)/game/components/GamePanel.tsx
+++ app/frontend/src/app/(onsite)/game/components/GamePanel.tsx
@@ -9,76 +9,6 @@ import { useGameContext } from "../contexts/gameContext";
 import { useTicTacToe } from "../../contexts/tictactoeContext";
 import { usePingPong } from "../../contexts/pingpongContext";
 
-import React from 'react';
-
-const AnimatedButton = () => {
-  const letters = ['B', 'U', 'T', 'T', 'O', 'N'];
-  
-  return (
-    <button className="
-      group relative
-      inline-block
-      border border-solid border-black
-      rounded-full
-      bg-black
-      py-[1.2rem] px-[3rem]
-      font-sans
-      text-base
-      font-extrabold
-      uppercase
-      leading-normal
-      text-white
-      overflow-hidden
-      select-none
-      cursor-pointer
-      box-border
-      [-webkit-tap-highlight-color:transparent]
-      [-webkit-appearance:button]
-      [-webkit-mask-image:-webkit-radial-gradient(#000,#fff)]
-      hover:outline-none
-    ">
-      {/* Original text layer */}
-      <div className="
-        absolute inset-0
-        grid place-content-center
-        bg-white text-black
-        transition-transform
-        duration-200
-        ease-[cubic-bezier(0.87,0,0.13,1)]
-        group-hover:translate-y-full
-      ">
-        Button
-      </div>
-      
-      {/* Animated letters */}
-      <div className="inline-flex">
-        {letters.map((letter, index) => (
-          <span 
-            key={index}
-            className={`
-              inline-block
-              opacity-0
-              ${
-                index % 2 === 0 
-                  ? '-translate-y-[15px]' 
-                  : 'translate-y-[15px]'
-              }
-              transition-all
-              duration-200
-              ease-[cubic-bezier(0.87,0,0.13,1)]
-              group-hover:opacity-100
-              group-hover:translate-y-0
-            `}
-            style={{ transitionDelay: `${index * 0.1}s` }}
-          >
-            {letter}
-          </span>
-        ))}
-      </div>
-    </button>
-  );
-};
-
 function GamePanel() {
 	const { gameType } = useGameContext();
 	const { connectivity: tConnect, setConnectivity: updatetConnect } = useTicTacToe();
@@ -103,7 +33,7 @@ function GamePanel() {
 					/>
 					{gameType === "pingpong" ? <PingPongOptions /> : <TicTacToeOptions />}
 				</motion.div>
-				<AnimatedButton />
+				<QueueButton onToggle={null}/>
 			</div>
 		</AnimatePresence>
 	);
