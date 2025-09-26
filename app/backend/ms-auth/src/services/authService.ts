be54c885015d8d7eb0c2eaa46fb1c849a4f1378f
Nabil Azouz <nazouz@student.1337.ma>
2025-09-26 02:54:33 +0100

    Frontend Refactor: refactored useForm custom hook, now it handles validation using a zod schema provided in arguments, updated SignUpForm to use the refactored version of useForm

--- app/backend/ms-auth/src/services/authService.ts
+++ app/backend/ms-auth/src/services/authService.ts
@@ -302,7 +302,7 @@ class AuthService {
 			
 			username: z.string()
 				.min(3, "Username must be at least 3 characters")
-				.max(50, "Username must be at least 50 characters")
+				.max(50, "Username must be at most 50 characters")
 				.regex(/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores"),
 		  
 			email: z.string()
