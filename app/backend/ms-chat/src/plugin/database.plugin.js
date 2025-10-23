697aebb4f0fdb7f55a0994c769298a4dd7426207
Ismail Assil <ismailassil00@gmail.com>
2025-10-23 07:04:45 +0100

    fixing the chat db path

--- app/backend/ms-chat/src/plugin/database.plugin.js
+++ app/backend/ms-chat/src/plugin/database.plugin.js
@@ -1,18 +1,23 @@
 import fp from 'fastify-plugin';
 import Database from 'better-sqlite3';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const ___filename = fileURLToPath(import.meta.url);
+const ___dirname = dirname(___filename);
+const __filePath = path.join(___dirname, "../../database/database.sqlite");
 
 async function dbConnector(fastify, options) {
-	const dbFile = './database/database.sqlite';
-	const db = new Database(dbFile, { verbose: console.log });
+	const db = new Database(__filePath, { verbose: console.log });
 
 	db.exec(`
-	CREATE TABLE IF NOT EXISTS message (
+		CREATE TABLE IF NOT EXISTS message (
 			senderId INTEGER NOT NULL,
 			receiverId INTEGER NOT NULL,
 			text TEXT NOT NULL,
 			created_at DATETIME DEFAULT (datetime('now', 'subsec'))
 		)
-		`);
+	`);
 	fastify.decorate('db', db);
 
 	fastify.addHook('onClose', (fastify, done) => {
