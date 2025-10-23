47d5c8b9e5bf769bac457578d63c62fb161d4e48
AmineMaila <aminemaila18@gmail.com>
2025-10-23 21:09:53 +0100

    Keep local database.db version

--- app/frontend/src/app/(onsite)/game/components/Items/Ping.tsx
+++ app/frontend/src/app/(onsite)/game/components/Items/Ping.tsx
@@ -14,9 +14,9 @@ const Ping = () => {
             className="absolute inset-0 flex items-center rounded-xl transition-all duration-150 border shadow-xl border-card bg-card hover:bg-white/5 hover:scale-[101%] active:scale-[99%] cursor-pointer [clip-path:polygon(0_0,55%_0,45%_100%,0_100%)] group"
             onClick={toggleSearch}
         >
-            <div className="absolute flex flex-col items-center left-3/11 max-w-[400px] min-w-0 min-h-0 max-h-[200px] -translate-x-1/2 ">
+            <div className="absolute flex p-5 flex-col items-center left-3/11 max-w-[400px] min-w-0 min-h-0 max-h-[200px] -translate-x-1/2 ">
                 <span
-                    className="uppercase pl-2 pt-2 tracking-widest text-4xl sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl 4xl:text-9xl whitespace-nowrap"
+                    className="uppercase pl-7 pt-2 tracking-widest text-4xl sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl 4xl:text-9xl whitespace-nowrap"
                     style={{
                         fontFamily: 'Serious2b'
                     }}
@@ -24,7 +24,7 @@ const Ping = () => {
                         ping
                 </span>
                 <span
-                    className={`flex justify-center sm:pl-2 sm:pt-2 w-full h-[60px] text-xs sm:text-sm md:text-md lg:text-lg 2xl:text-xl whitespace-nowrap
+                    className={`flex justify-center font-extralight sm:pl-2 sm:pt-2 w-full h-[60px] text-xs sm:text-sm md:text-md lg:text-lg 2xl:text-xl whitespace-nowrap
                     ${isSearching 
                         ? "scale-100 cursor-pointer pl-6 transition-transform duration-200"
                         : "scale-105 cursor-pointer pl-6 transition-transform duration-200"
--- app/frontend/src/app/(onsite)/game/components/Items/Pong.tsx
+++ app/frontend/src/app/(onsite)/game/components/Items/Pong.tsx
@@ -12,7 +12,7 @@ const Pong = () => {
         className="absolute inset-0 flex items-center rounded-xl transition-all duration-150 border shadow-xl border-card bg-card hover:bg-white/5  hover:scale-[101%] active:scale-[99%] cursor-pointer [clip-path:polygon(55%_0,100%_0,100%_100%,45%_100%)] group"
         onClick={handleClick}
         >
-            <div className="absolute flex flex-col justify-end items-center right-3/11 max-w-[400px] min-w-0 min-h-0 max-h-[200px] translate-x-1/2 ">
+            <div className="absolute flex flex-col p-5 justify-end items-center right-3/11 max-w-[400px] min-w-0 min-h-0 max-h-[200px] translate-x-1/2 ">
                 <span
                     className="flex justify-center items-end sm:pr-2 sm:pb-2 w-full h-[60px] text-xs sm:text-sm md:text-md lg:text-lg 2xl:text-xl whitespace-nowrap"
                     style={{
@@ -22,7 +22,7 @@ const Pong = () => {
                         Local Play
                 </span>
                 <span
-                    className="uppercase pl-2 pt-2 text-4xl sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl 4xl:text-9xl whitespace-nowrap"
+                    className="uppercase pl-7 pt-2 text-4xl sm:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl 4xl:text-9xl whitespace-nowrap"
                     style={{
                         fontFamily: 'Serious2b'
                     }}
